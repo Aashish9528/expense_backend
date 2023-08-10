8b120314414f369bfa526fe59dda4bedd7c93e12
Aashish9528 <110108913+Aashish9528@users.noreply.github.com>
2023-08-10 20:23:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ const PORT = process.env.PORT
 //middlewares
 app.use(express.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname,'./client/build')))
+// app.use(express.static(path.join(__dirname,'./client/build')))
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,"./client/build/index.html"))
-  })
+// app.use('*',function(req,res){
+//     res.sendFile(path.join(__dirname,"./client/build/index.html"))
+//   })
+app.get("/",(req,res)=>{
+    res.setHeader("Access-Control-Allow-Credentials","true")
+})
 const server = () => {
     db()
     app.listen(PORT, () => {
