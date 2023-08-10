e04611eb4ea19e92280ff5054c9c36480da4d568
Aashish9528 <110108913+Aashish9528@users.noreply.github.com>
2023-08-10 20:16:07 +0530

    c

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
+const path=require('path');
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname,'./client/build')))
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+  })
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
